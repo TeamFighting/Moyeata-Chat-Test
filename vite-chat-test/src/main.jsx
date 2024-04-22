c18d441057a8c0552b59da05737de874c97c4381
Jake-huen <tae77777@naver.com>
2024-04-22 20:27:42 +0900

    Vite APP

---
+++ vite-chat-test/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ChatRoom from './ChatRoom.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChatRoom />
+  </React.StrictMode>
+)
